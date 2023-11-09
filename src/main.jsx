3c379d7038ad803567cb507b2ed66e91562326eb
micaaranguiz <micaelaaranguiz2@gmail.com>
2023-11-09 18:59:47 -0300

    En teoria está terminado consumiendo una api

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Link } from 'react-router-dom';
+import { Button } from 'primereact/button';
 
 
 
@@ -17,21 +19,30 @@ const estilos = {
         },
         panelderecho: {
                 width: "10%",
-                height: "967px",
+                height: "966px",
                 float: "left",
                 backgroundColor: "#888BE3",
-                marginTop: "-3.8%",
+                marginTop: "-3.7%",
                 marginRight: "10px"
 
         },
         panelizquierdo: {
                 width: "10%",
-                height: "967px",
+                height: "966px",
                 float: "right",
                 backgroundColor: "#888BE3",
-                marginTop: "-3.8%",
+                marginTop: "-3.7%",
                 marginLeft: "10px"
-        }
+        },
+        enviar: {
+                margin: "10px"
+              },
+              boton:{
+                margin: "10px",
+              
+                    borderRadius: 5,
+                    fontSize:"15px"
+              }
 
 }
 
@@ -39,10 +50,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <>
                 <h1 style={estilos.titulo}>Nuestra aplicación de posteos</h1>
                 <div style={estilos.panelderecho}>
-                        Holis
+                
+                <a href='/users' > <Button label="Usuarios Creados " style={estilos.boton}/></a><br />               
+                <a href='/posts' > <Button label="Publicaciones Creadas "style={estilos.boton} /></a><br />    
+                
                 </div>
                 <div style={estilos.panelizquierdo}>
-                        Holis
+                <a href='/newuser' > <Button label="Crear Nuevo Usuario " style={estilos.boton}/></a><br />               
+                <a href='/newpost' > <Button label="Crear Nueva Publicacion" style={estilos.boton} /></a><br />    
+                
                 </div>
                 <App />
 
